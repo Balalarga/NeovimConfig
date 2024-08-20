f8a86ffe62f77f6ac5fc095815a9092311d3be73
Andrey Beyresh <a_beyresh@lesta.group>
2024-08-20 18:45:30 +0300

    Add transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
     "rose-pine/neovim",
     config = function()
+        require('rose-pine').setup({
+            styles = {
+                transparency = true,
+            }
+        })
         vim.cmd([[colorscheme rose-pine]])
     end,
 }
